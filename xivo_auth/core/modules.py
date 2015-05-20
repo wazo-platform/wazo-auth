4d3c1172f9c69adb6743b0ffc7c7aa903a2cb5db
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-05-20 11:06:11 -0400

    add the base class for auth backends

--- xivo_auth/core/modules.py
+++ xivo_auth/core/modules.py
@@ -1,6 +1,22 @@
 import abc
 
 
+class BaseAuthenticationBackend(object):
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, config):
+        pass
+
+    @abc.abstractmethod
+    def get_uuid(self, identifier):
+        """Returns the uuid of the given user's identifier"""
+
+    @abc.abstractmethod
+    def verify_password(self, identifier, passwd):
+        """Returns True or False for the given user's identifier/password combination"""
+
+
 class BasePlugin(object):
 
     __metaclass__ = abc.ABCMeta
