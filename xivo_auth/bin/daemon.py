a245d5ff892b23f18843be5bd420bd4252504802
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-05-27 08:10:47 -0400

    extract a Controller class from the main()
    move the process/server starting and plugin loading from the main
    function to a helper class

--- xivo_auth/bin/daemon.py
+++ xivo_auth/bin/daemon.py
@@ -91,39 +91,51 @@ def _get_config():
     return ChainMap(reinterpreted_config, cli_config, file_config, _DEFAULT_CONFIG)
 
 
+class _Controller(object):
+
+    def __init__(self, config):
+        try:
+            self._listen_addr = config['rest_api']['listen']
+            self._listen_port = config['rest_api']['port']
+        except KeyError:
+            logger.error('Missing configuration to start the HTTP application')
+
+        self._app = create_app()
+        self._app.config.update(config)
+
+        load_cors(self._app, config['rest_api'])
+
+        extensions.celery = make_celery(self._app)
+        extensions.consul = Consul(host=config['consul']['host'],
+                                   port=config['consul']['port'],
+                                   token=config['consul']['token'])
+
+        register_signal_handlers(self._app)
+
+        backends = plugin_manager.load_plugins(self._app, config)
+        self._app.config['backends'] = backends
+        self._app.register_blueprint(http.auth)
+
+        sys.argv = [sys.argv[0]]  # For the celery process
+        self._celery_iface = CeleryInterface(extensions.celery)
+
+    def run(self):
+        self._celery_iface.start()
+        self._app.run(self._listen_addr, self._listen_port)
+        self._celery_iface.join()
+
+
 def main():
     config = _get_config()
 
     setup_logging(config['log_filename'], config['foreground'], config['debug'], config['log_level'])
-    user = config['user']
 
+    user = config.get('user')
     if user:
         change_user(user)
 
-    application = create_app()
-    application.config.update(config)
-
-    load_cors(application, config['rest_api'])
-
-    extensions.celery = make_celery(application)
-    extensions.consul = Consul(host=config['consul']['host'],
-                               port=config['consul']['port'],
-                               token=config['consul']['token'])
-
-    register_signal_handlers(application)
-
-    backends = plugin_manager.load_plugins(application, config)
-    application.config['backends'] = backends
-    application.register_blueprint(http.auth)
-
-    sys.argv = [sys.argv[0]]
-    celery_interface = CeleryInterface(extensions.celery)
-    celery_interface.start()
-
-    application.run(config['rest_api']['listen'],
-                    config['rest_api']['port'])
-
-    celery_interface.join()
+    controller = _Controller(config)
+    controller.run()
 
 
 def register_signal_handlers(application):
