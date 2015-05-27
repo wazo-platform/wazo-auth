228dcb0c775777cb874cc43efc0b5558f1ff69c5
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-05-27 07:47:14 -0400

    remove an unused AbstractClass

--- xivo_auth/__init__.py
+++ xivo_auth/__init__.py
@@ -16,7 +16,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from blinker import Namespace
-from core.modules import BasePlugin  # noqa
 from core.modules import BaseAuthenticationBackend  # noqa
 
 _signals = Namespace()
--- xivo_auth/core/modules.py
+++ xivo_auth/core/modules.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 import abc
 
 
@@ -15,15 +32,3 @@ class BaseAuthenticationBackend(object):
     @abc.abstractmethod
     def verify_password(self, identifier, passwd):
         """Returns True or False for the given user's identifier/password combination"""
-
-
-class BasePlugin(object):
-
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def load(self, app, config):
-        pass
-
-    def unload(self):
-        pass
