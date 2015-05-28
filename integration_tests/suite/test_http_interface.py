8f6dd5586059b1287a21c8d840b66ef4f3721170
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2015-05-28 15:11:40 -0400

    split tests into many test cases

--- integration_tests/suite/test_http_interface.py
+++ integration_tests/suite/test_http_interface.py
@@ -34,26 +34,40 @@ logger = logging.getLogger(__name__)
 ISO_DATETIME = '%Y-%m-%dT%H:%M:%S.%f'
 
 
-class TestTokenCreation(unittest.TestCase):
+class AssetRunner(object):
 
-    asset = 'mock_backend'
-    launcher = 'docker-compose'
-    url = 'http://localhost:9497/0.1/token'
+    _launcher = 'docker-compose'
+    _instance = None
 
-    @classmethod
-    def launch_services(cls):
-        cls.container_name = cls.asset
-        asset_path = os.path.join(os.path.dirname(__file__), '..', 'assets', cls.asset)
-        cls.cur_dir = os.getcwd()
+    def __init__(self):
+        self._running_asset = None
+
+    def __del__(self):
+        if self._running_asset:
+            self._stop(self._running_asset)
+
+    def start(self, asset):
+        if asset == self._running_asset:
+            return
+        elif self._running_asset != asset:
+            self._stop(asset)
+        self._start(asset)
+
+    def _start(self, asset):
+        self._running_asset = asset
+        asset_path = os.path.join(os.path.dirname(__file__), '..', 'assets', asset)
+        self.cur_dir = os.getcwd()
         os.chdir(asset_path)
-        cls._run_cmd('{} up -d'.format(cls.launcher))
+        self._run_cmd('{} up -d'.format(self._launcher))
         time.sleep(1)
 
-    @classmethod
-    def stop_services(cls):
-        cls._run_cmd('{} kill'.format(cls.launcher))
-        cls._run_cmd('{} rm --force'.format(cls.launcher))
-        os.chdir(cls.cur_dir)
+    def _stop(self, asset):
+        if not asset or asset != self._running_asset:
+            return
+
+        self._run_cmd('{} kill'.format(self._launcher))
+        self._run_cmd('{} rm --force'.format(self._launcher))
+        os.chdir(self.cur_dir)
         time.sleep(1)
 
     @staticmethod
@@ -63,12 +77,20 @@ class TestTokenCreation(unittest.TestCase):
         logger.info('%s', out)
 
     @classmethod
-    def setUpClass(cls):
-        cls.launch_services()
+    def get_instance(cls):
+        if not cls._instance:
+            cls._instance = AssetRunner()
+        return cls._instance
+
+
+class _BaseTestCase(unittest.TestCase):
+
+    url = 'http://localhost:9497/0.1/token'
 
     @classmethod
-    def tearDownClass(cls):
-        cls.stop_services()
+    def setUpClass(cls):
+        cls._asset_runner = AssetRunner.get_instance()
+        cls._asset_runner.start(cls.asset)
 
     def _post_token(self, username, password, backend=None, expiration=None):
         if not backend:
@@ -76,10 +98,74 @@ class TestTokenCreation(unittest.TestCase):
         s = requests.Session()
         s.headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
         s.auth = requests.auth.HTTPBasicAuth(username, password)
-        data = {'type': backend}
+        data = {'backend': backend}
         if expiration:
             data['expiration'] = expiration
         return s.post(self.url, data=json.dumps(data))
+
+
+class TestHEADToken(_BaseTestCase):
+
+    asset = 'mock_backend'
+
+    def test_that_head_with_a_valid_token_returns_204(self):
+        token = self._post_token('foo', 'bar').json()['data']['token']
+
+        response = requests.head('{}/{}'.format(self.url, token))
+
+        assert_that(response.status_code, equal_to(204))
+
+    def test_that_head_with_an_invalid_token_returns_403(self):
+        response = requests.head('{}/{}'.format(self.url, 'abcdef'))
+
+        assert_that(response.status_code, equal_to(404))
+
+
+class TestGETBackends(_BaseTestCase):
+
+    asset = 'mock_backend'
+
+    def test_backends(self):
+        response = requests.get('http://localhost:9497/0.1/backends')
+
+        assert_that(response.json()['data'],
+                    contains_inanyorder('mock', 'broken_init', 'broken_verify_password'))
+
+
+class TestGETToken(_BaseTestCase):
+
+    asset = 'mock_backend'
+
+    def test_that_get_returns_the_uuid(self):
+        token = self._post_token('foo', 'bar').json()['data']['token']
+
+        response = requests.get('{}/{}'.format(self.url, token))
+
+        assert_that(response.status_code, equal_to(200))
+        assert_that(response.json()['data']['uuid'], equal_to('a-mocked-uuid'))
+
+
+class TestDELETEToken(_BaseTestCase):
+
+    asset = 'mock_backend'
+
+    def test_that_get_does_not_work_after_delete(self):
+        token = self._post_token('foo', 'bar').json()['data']['token']
+
+        requests.delete('{}/{}'.format(self.url, token))
+        response = requests.get('{}/{}'.format(self.url, token))
+
+        assert_that(response.status_code, equal_to(404))
+
+    def test_that_deleting_unexistant_token_returns_200(self):
+        response = requests.delete('{}/{}'.format(self.url, 'not-a-valid-token'))
+
+        assert_that(response.status_code, equal_to(200))
+
+
+class TestTokenPost(_BaseTestCase):
+
+    asset = 'mock_backend'
 
     def test_that_the_wrong_password_returns_401(self):
         response = self._post_token('foo', 'not_bar')
@@ -88,17 +174,6 @@ class TestTokenCreation(unittest.TestCase):
 
     def test_that_the_right_credentials_return_a_token(self):
         response = self._post_token('foo', 'bar')
-        content = response.json()['data']
-        token = content['token']
-
-        assert_that(response.status_code, equal_to(200))
-        assert_that(token, has_length(36))
-
-    def test_that_the_right_credentials_no_headers(self):
-        s = requests.Session()
-        s.auth = requests.auth.HTTPBasicAuth('foo', 'bar')
-        payload = json.dumps({'type': 'mock'})
-        response = s.post(self.url, data=payload)
         content = response.json()['data']
         token = content['token']
 
@@ -138,45 +213,6 @@ class TestTokenCreation(unittest.TestCase):
         response = self._post_token('foo', 'bar', expiration=-1)
 
         assert_that(response.status_code, equal_to(400))
-
-    def test_that_head_with_a_valid_token_returns_204(self):
-        token = self._post_token('foo', 'bar').json()['data']['token']
-
-        response = requests.head('{}/{}'.format(self.url, token))
-
-        assert_that(response.status_code, equal_to(204))
-
-    def test_that_head_with_an_invalid_token_returns_403(self):
-        response = requests.head('{}/{}'.format(self.url, 'abcdef'))
-
-        assert_that(response.status_code, equal_to(404))
-
-    def test_backends(self):
-        response = requests.get('http://localhost:9497/0.1/backends')
-
-        assert_that(response.json()['data'],
-                    contains_inanyorder('mock', 'broken_init', 'broken_verify_password'))
-
-    def test_that_get_returns_the_uuid(self):
-        token = self._post_token('foo', 'bar').json()['data']['token']
-
-        response = requests.get('{}/{}'.format(self.url, token))
-
-        assert_that(response.status_code, equal_to(200))
-        assert_that(response.json()['data']['uuid'], equal_to('a-mocked-uuid'))
-
-    def test_that_get_does_not_work_after_delete(self):
-        token = self._post_token('foo', 'bar').json()['data']['token']
-
-        requests.delete('{}/{}'.format(self.url, token))
-        response = requests.get('{}/{}'.format(self.url, token))
-
-        assert_that(response.status_code, equal_to(404))
-
-    def test_that_deleting_unexistant_token_returns_200(self):
-        response = requests.delete('{}/{}'.format(self.url, 'not-a-valid-token'))
-
-        assert_that(response.status_code, equal_to(200))
 
     def test_that_expired_tokens_are_not_valid(self):
         token = self._post_token('foo', 'bar', expiration=1).json()['data']['token']
