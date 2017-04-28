62a695af3504d389d3046adfe1ba7648d44bbdf1
Sylvain Boily <sboily@proformatique.com>
2017-04-28 13:35:33 -0400

    add access to websocketd for root user

---
+++ alembic/versions/50de5600b678_add_websocket_and_events_access_to_root_.py
@@ -0,0 +1,101 @@
+"""Add websocket and events access to root user
+
+Revision ID: 50de5600b678
+Revises: 52a21739a22a
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '50de5600b678'
+down_revision = '52a21739a22a'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+POLICY_NAME = 'wazo_default_admin_policy'
+ACL_TEMPLATES = [
+    'websocketd',
+    'events.#'
+]
+
+policy_table = sa.sql.table(
+    'auth_policy',
+    sa.Column('uuid', sa.String(38)),
+    sa.Column('name', sa.String(80)),
+)
+acl_template_table = sa.sql.table(
+    'auth_acl_template',
+    sa.Column('id', sa.Integer),
+    sa.Column('template', sa.Text),
+)
+policy_template = sa.sql.table(
+    'auth_policy_template',
+    sa.Column('policy_uuid', sa.String(38)),
+    sa.Column('template_id', sa.Integer),
+)
+
+
+def _find_acl_template(conn, acl_template):
+    query = sa.sql.select(
+        [acl_template_table.c.id]
+    ).where(
+        acl_template_table.c.template == acl_template,
+    ).limit(1)
+    return conn.execute(query).scalar()
+
+
+def _find_acl_templates(conn, acl_templates):
+    acl_template_ids = []
+    for acl_template in acl_templates:
+        acl_template_id = _find_acl_template(conn, acl_template)
+        if acl_template_id:
+            acl_template_ids.append(acl_template_id)
+    return acl_template_ids
+
+
+def _get_policy_uuid(conn, policy_name):
+    policy_query = sa.sql.select(
+        [policy_table.c.uuid]
+    ).where(policy_table.c.name == policy_name)
+
+    for policy in conn.execute(policy_query).fetchall():
+        return policy[0]
+
+
+def _insert_acl_template(conn, acl_templates):
+    acl_template_ids = []
+    for acl_template in acl_templates:
+        acl_template_id = _find_acl_template(conn, acl_template)
+        if not acl_template_id:
+            query = acl_template_table.insert().returning(acl_template_table.c.id).values(
+                template=acl_template,
+            )
+            acl_template_id = conn.execute(query).scalar()
+        acl_template_ids.append(acl_template_id)
+    return acl_template_ids
+
+
+def upgrade():
+    conn = op.get_bind()
+    policy_uuid = _get_policy_uuid(conn, POLICY_NAME)
+    acl_template_ids = _insert_acl_template(conn, ACL_TEMPLATES)
+    op.bulk_insert(
+        policy_template,
+        [{'policy_uuid': policy_uuid,
+          'template_id': template_id} for template_id in acl_template_ids],
+    )
+
+
+def downgrade():
+    conn = op.get_bind()
+    acl_template_ids = _find_acl_templates(conn, ACL_TEMPLATES)
+    if acl_template_ids:
+        policy_uuid = _get_policy_uuid(conn, POLICY_NAME)
+        delete_query = policy_template.delete(
+        ).where(
+            sa.sql.and_(
+                policy_template.c.policy_uuid == policy_uuid,
+                policy_template.c.template_id.in_(acl_template_ids)))
+        op.execute(delete_query)
