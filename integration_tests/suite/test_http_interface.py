aa419b6719569d56081001623f88aca0e5995c58
Sebastien Duthil <sduthil@wazo.community>
2017-05-10 11:02:14 -0400

    remove unused imports

--- integration_tests/suite/test_http_interface.py
+++ integration_tests/suite/test_http_interface.py
@@ -15,15 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-import subprocess
-import unittest
 import time
 import os
 import uuid
-import json
 import logging
 
-from contextlib import contextmanager
 from datetime import datetime, timedelta
 
 import requests
