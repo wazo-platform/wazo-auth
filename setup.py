0a50ac0451a721b915bfc72acc5a08eb03ba6afe
Pascal Cadotte Michaud <pcm@wazo.community>
2017-10-13 10:48:04 -0400

    move the launcher from bin/daemon.py to main.py

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
 
     entry_points={
         'console_scripts': [
-            'wazo-auth=wazo_auth.bin.daemon:main',
+            'wazo-auth=wazo_auth.main:main',
         ],
         'wazo_auth.backends': [
             'xivo_admin = wazo_auth.plugins.backends:XiVOAdmin',
