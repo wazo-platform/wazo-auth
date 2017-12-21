0b1ef4f4cf4654d645a0736d43abebe7fc302d91
Pascal Cadotte Michaud <pcm@wazo.io>
2017-12-21 11:21:34 -0500

    init: move some of the initialization to the post
    I realized that the __init__ of the instance is called at each call

--- wazo_auth/plugins/http/init/http.py
+++ wazo_auth/plugins/http/init/http.py
@@ -2,44 +2,47 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
 import os
 
 from flask import request
 from wazo_auth import exceptions, http
 from .schemas import InitPostSchema
 
+logger = logging.getLogger(__name__)
+
 
 class Init(http.ErrorCatchingResource):
 
     def __init__(self, policy_service, user_service, config):
-        self.user_service = user_service
+        self._user_service = user_service
+        self._policy_service = policy_service
         self._init_key_filename = config['init_key_filename']
-        policy_name = config['init_policy_name']
-        self._policy_uuid = policy_service.list(name=policy_name)[0]['uuid']
-        self._key = None
-        with open(self._init_key_filename, 'r') as f:
-            for line in f:
-                self._key = line.strip()
-                break
+        self._policy_name = config['init_policy_name']
 
     def post(self):
         args, errors = InitPostSchema().load(request.get_json())
         if errors:
             raise exceptions.InitParamException.from_errors(errors)
 
-        if not self._key:
+        try:
+            with open(self._init_key_filename, 'r') as f:
+                for line in f:
+                    key = line.strip()
+                    break
+        except IOError:
             raise exceptions.AuthenticationFailedException()
 
-        if args.pop('key') != self._key:
+        if args.pop('key') != key:
             raise exceptions.AuthenticationFailedException()
 
-        result = self.user_service.new_user(**args)
-        self.user_service.add_policy(result['uuid'], self._policy_uuid)
+        result = self._user_service.new_user(**args)
+        policy_uuid = self._policy_service.list(name=self._policy_name)[0]['uuid']
+        self._user_service.add_policy(result['uuid'], policy_uuid)
 
-        self._key = None
         try:
             os.remove(self._init_key_filename)
         except OSError:
-            pass
+            logger.info('failed to remove the key file')
 
         return result, 200
