907a94b2029ae630e2baa7347f9ab4f3c27e4f12
Pascal Cadotte Michaud <pcm@wazo.io>
2017-12-27 11:35:56 -0500

    add a test for GET email confirmation

--- integration_tests/suite/test_email_confirmation.py
+++ integration_tests/suite/test_email_confirmation.py
@@ -2,7 +2,8 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-from hamcrest import assert_that, contains, has_entries
+import requests
+from hamcrest import assert_that, contains, equal_to, has_entries
 from .helpers import fixtures
 from .helpers.base import assert_http_error, assert_no_error, MockBackendTestCase, UNKNOWN_UUID
 
@@ -15,6 +16,21 @@ class TestEmailConfirmation(MockBackendTestCase):
 
         assert_http_error(404, self.client.emails.confirm, UNKNOWN_UUID)
         assert_no_error(self.client.emails.confirm, email_uuid)
+
+        updated_user = self.client.users.get(user['uuid'])
+        assert_that(
+            updated_user,
+            has_entries(emails=contains(has_entries(
+                address='foobar@example.com',
+                confirmed=True))))
+
+    @fixtures.http_user_register(email_address='foobar@example.com')
+    def test_email_confirmation_get(self, user):
+        email_uuid = user['emails'][0]['uuid']
+        url = 'https://{}:{}/0.1/emails/{}/confirm'.format(self.get_host(), self._auth_port, email_uuid)
+        token = self.client.token.new(backend='mock', expiration=3600)['token']
+        response = requests.get(url, params={'token': token}, verify=False)
+        assert_that(response.status_code, equal_to(204))
 
         updated_user = self.client.users.get(user['uuid'])
         assert_that(
