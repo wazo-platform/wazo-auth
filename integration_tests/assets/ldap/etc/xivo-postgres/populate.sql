d65ae5f37b5f4fdedeb5ec7c245648ae1292ea1f
Pascal Cadotte Michaud <pcm@wazo.io>
2018-03-16 14:44:21 -0400

    add the tenant to the db setup

--- integration_tests/assets/ldap/etc/xivo-postgres/populate.sql
+++ integration_tests/assets/ldap/etc/xivo-postgres/populate.sql
@@ -1,3 +1,5 @@
+INSERT INTO "tenant" (uuid) VALUES ('4a037de3-94bd-4d40-b4d1-3fc09184c3d2');
+INSERT INTO "entity" ("id", "name", "displayname", "tenant_uuid", "description") VALUES (1, 'default', 'Default', '4a037de3-94bd-4d40-b4d1-3fc09184c3d2', '');
 INSERT INTO "func_key_template" (private) VALUES (TRUE);
 INSERT INTO "userfeatures" (uuid, firstname, email, description, func_key_private_template_id, entityid)
 VALUES
