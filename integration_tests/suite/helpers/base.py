386300941499ea0a42d5f86f5ace3a585541f0b8
Pascal Cadotte Michaud <pcm@wazo.io>
2018-04-26 13:25:06 -0400

    tests: test GET on /token with an unauthorized tenant

--- integration_tests/suite/helpers/base.py
+++ integration_tests/suite/helpers/base.py
@@ -136,16 +136,19 @@ class BaseTestCase(AuthLaunchingTestCase):
         else:
             self.fail('Should have raised an exception')
 
-    def _get_token(self, token, acls=None):
+    def _get_token(self, token, acls=None, tenant=None):
         client = self.new_auth_client()
         args = {}
         if acls:
             args['required_acl'] = acls
+        if tenant:
+            args['tenant'] = tenant
+
         return client.token.get(token, **args)
 
-    def _get_token_with_expected_exception(self, token, acls=None, status_code=None, msg=None):
+    def _get_token_with_expected_exception(self, token, acls=None, tenant=None, status_code=None, msg=None):
         try:
-            self._get_token(token, acls)
+            self._get_token(token, acls, tenant)
         except requests.HTTPError as e:
             if status_code:
                 assert_that(e.response.status_code, equal_to(status_code))
