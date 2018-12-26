8a98ebbef1cec2bd284ae35cb9899267cf0fbc86
Sebastien Duthil <sduthil@wazo.community>
2018-12-26 11:32:10 -0500

    tests: add missing file

---
+++ integration_tests/suite/helpers/database.py
@@ -0,0 +1,56 @@
+# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import logging
+import sqlalchemy as sa
+
+logger = logging.getLogger(__name__)
+
+
+class Database:
+
+    @classmethod
+    def build(cls, user, password, host, port, db):
+        tpl = "postgresql://{user}:{password}@{host}:{port}"
+        uri = tpl.format(user=user,
+                         password=password,
+                         host=host,
+                         port=port)
+        return cls(uri, db)
+
+    def __init__(self, uri, db):
+        self.uri = uri
+        self.db = db
+
+    def is_up(self):
+        try:
+            self.connect()
+            return True
+        except Exception as e:
+            logger.debug('Database is down: %s', e)
+            return False
+
+    def create_engine(self, db=None, isolate=False):
+        db = db or self.db
+        uri = "{}/{}".format(self.uri, db)
+        if isolate:
+            return sa.create_engine(uri, isolation_level='AUTOCOMMIT')
+        return sa.create_engine(uri)
+
+    def connect(self, db=None):
+        db = db or self.db
+        return self.create_engine(db).connect()
+
+    def recreate(self):
+        engine = self.create_engine("postgres", isolate=True)
+        connection = engine.connect()
+        connection.execute("""
+                           SELECT pg_terminate_backend(pg_stat_activity.pid)
+                           FROM pg_stat_activity
+                           WHERE pg_stat_activity.datname = '{db}'
+                           AND pid <> pg_backend_pid()
+                           """.format(db=self.db))
+        connection.execute("DROP DATABASE IF EXISTS {db}".format(db=self.db))
+        connection.execute("CREATE DATABASE {db} TEMPLATE {template}".format(db=self.db,
+                                                                             template=self.TEMPLATE))
+        connection.close()
