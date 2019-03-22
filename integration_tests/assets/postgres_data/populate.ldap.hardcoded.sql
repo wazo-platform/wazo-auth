aa8ef28c6a33bd8fce4c4b05c5eb18f395bb7e1b
Pascal Cadotte Michaud <pcm@wazo.io>
2019-03-22 13:08:18 -0400

    test populate: remove the entity table

--- integration_tests/assets/postgres_data/populate.ldap.hardcoded.sql
+++ integration_tests/assets/postgres_data/populate.ldap.hardcoded.sql
@@ -1,6 +1,5 @@
 INSERT INTO "tenant" (uuid) VALUES ('4a037de3-94bd-4d40-b4d1-3fc09184c3d2') ON CONFLICT DO NOTHING;
-INSERT INTO "entity" ("id", "name", "displayname", "tenant_uuid", "description") VALUES (1, 'default', 'Default', '4a037de3-94bd-4d40-b4d1-3fc09184c3d2', '');
 INSERT INTO "func_key_template" (private) VALUES (TRUE);
-INSERT INTO "userfeatures" (uuid, firstname, email, description, func_key_private_template_id, entityid, tenant_uuid)
+INSERT INTO "userfeatures" (uuid, firstname, email, description, func_key_private_template_id, tenant_uuid)
 VALUES
-(1, 'Alice', 'awonderland@wazo-auth.com', '', 1, 1, '4a037de3-94bd-4d40-b4d1-3fc09184c3d2');
+(1, 'Alice', 'awonderland@wazo-auth.com', '', 1, '4a037de3-94bd-4d40-b4d1-3fc09184c3d2');
--- integration_tests/assets/postgres_data/populate.ldap.sql
+++ integration_tests/assets/postgres_data/populate.ldap.sql
@@ -1,6 +1,5 @@
 INSERT INTO "tenant" (uuid) VALUES ((SELECT uuid FROM auth_tenant WHERE uuid = parent_uuid)) ON CONFLICT DO NOTHING;
-INSERT INTO "entity" ("name", "displayname", "tenant_uuid", "description") VALUES ('default', 'Default', (SELECT uuid FROM auth_tenant WHERE uuid = parent_uuid), '');
 INSERT INTO "func_key_template" (private) VALUES (TRUE);
-INSERT INTO "userfeatures" (uuid, firstname, email, description, func_key_private_template_id, entityid, tenant_uuid)
+INSERT INTO "userfeatures" (uuid, firstname, email, description, func_key_private_template_id, tenant_uuid)
 VALUES
-(1, 'Alice', 'awonderland@wazo-auth.com', '', 1, (SELECT id FROM entity ORDER BY id DESC LIMIT 1), (SELECT uuid FROM auth_tenant WHERE uuid = parent_uuid));
+(1, 'Alice', 'awonderland@wazo-auth.com', '', 1, (SELECT uuid FROM auth_tenant WHERE uuid = parent_uuid));
