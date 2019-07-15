9d46f971845aa154f1960d1062d2d6ce28b36ae8
Pascal Cadotte Michaud <pcm@wazo.io>
2019-07-15 14:16:40 -0400

    google: copy the plugin from wazo-google

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
             'user_session = wazo_auth.plugins.http.user_session.plugin:Plugin',
         ],
         'wazo_auth.external_auth': [
+            'google = wazo_auth.plugins.external_auth.google.plugin:GooglePlugin',
             'microsoft = wazo_auth.plugins.external_auth.microsoft.plugin:MicrosoftPlugin',
             'mobile = wazo_auth.plugins.external_auth.mobile.plugin:Plugin',
         ],
