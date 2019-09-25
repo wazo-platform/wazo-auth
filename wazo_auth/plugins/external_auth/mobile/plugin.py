ecbbfce0142a60809b448af2aab073bebd7173fc
Pascal Cadotte Michaud <pcm@wazo.io>
2019-09-25 10:41:43 -0400

    fix license missmatch

--- wazo_auth/plugins/external_auth/mobile/plugin.py
+++ wazo_auth/plugins/external_auth/mobile/plugin.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from .http import MobileAuth, MobileAuthSenderID
 
