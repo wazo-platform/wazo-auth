904c80a4d38357e64e1c3033678f26904463bfb7
Pascal Cadotte Michaud <pcm@wazo.io>
2019-11-11 10:26:38 -0500

    rebase alembic revision

--- alembic/versions/ac5b4f05aebb_add_mobile_to_refresh_tokens.py
+++ alembic/versions/ac5b4f05aebb_add_mobile_to_refresh_tokens.py
@@ -1,7 +1,7 @@
 """add mobile to refresh tokens
 
 Revision ID: ac5b4f05aebb
-Revises: f6df424cb3b8
+Revises: 2e9c110c81aa
 
 """
 
@@ -10,17 +10,21 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'ac5b4f05aebb'
-down_revision = 'f6df424cb3b8'
+down_revision = '2e9c110c81aa'
 
 
 def upgrade():
     # All existing refresh tokens are mobile
     op.add_column(
         'auth_refresh_token',
-        sa.Column('mobile', sa.Boolean, nullable=False, default=True, server_default='true'),
+        sa.Column(
+            'mobile', sa.Boolean, nullable=False, default=True, server_default='true'
+        ),
     )
     # After the migration the default for new refresh token mobile is False
-    op.alter_column('auth_refresh_token', 'mobile', nullable=False, server_default='false')
+    op.alter_column(
+        'auth_refresh_token', 'mobile', nullable=False, server_default='false'
+    )
 
 
 def downgrade():
