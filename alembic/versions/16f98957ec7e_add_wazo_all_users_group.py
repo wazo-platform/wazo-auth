e3f70e4720904fa9b59cfb744ba8c2c77eeea414
Sebastien Duthil <sduthil@wazo.community>
2020-09-03 16:07:58 -0400

    users: add existing users to wazo-all-users group

--- alembic/versions/16f98957ec7e_add_wazo_all_users_group.py
+++ alembic/versions/16f98957ec7e_add_wazo_all_users_group.py
@@ -16,6 +16,17 @@ group_table = sa.sql.table(
     'auth_group', sa.Column('uuid'), sa.Column('tenant_uuid'), sa.Column('name')
 )
 tenant_table = sa.sql.table('auth_tenant', sa.Column('uuid'), sa.Column('parent_uuid'))
+user_group_table = sa.sql.table(
+    'auth_user_group',
+    sa.Column('user_uuid'),
+    sa.Column('group_uuid'),
+    sa.Column('tenant_uuid'),
+)
+user_table = sa.sql.table(
+    'auth_user',
+    sa.Column('uuid'),
+    sa.Column('tenant_uuid', None, sa.ForeignKey('auth_tenant.uuid')),
+)
 
 
 def all_tenant_uuids():
@@ -42,9 +53,28 @@ def delete_wazo_all_users_group(tenant_uuid):
     op.execute(query)
 
 
+def add_all_users_in_group(group_uuids):
+    group_members_query = (
+        sa.sql.select([user_table.c.uuid, group_table.c.uuid])
+        .select_from(
+            user_table.join(
+                tenant_table, user_table.c.tenant_uuid == tenant_table.c.uuid
+            ).join(group_table, tenant_table.c.uuid == group_table.c.tenant_uuid)
+        )
+        .where(group_table.c.uuid.in_(group_uuids))
+    )
+    query = user_group_table.insert().from_select(
+        [user_group_table.c.user_uuid, user_group_table.c.group_uuid],
+        group_members_query,
+    )
+    op.get_bind().execute(query)
+
+
 def upgrade():
+    group_uuids = set()
     for tenant_uuid in all_tenant_uuids():
-        group_uuid = create_wazo_all_users_group(tenant_uuid)
+        group_uuids.add(create_wazo_all_users_group(tenant_uuid))
+    add_all_users_in_group(group_uuids)
 
 
 def downgrade():
