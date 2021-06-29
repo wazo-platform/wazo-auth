2cd56b2261868509d927403c335be4f3aed841d8
Francois Blackburn <fblackburn@wazo.community>
2021-06-29 16:08:40 -0400

    policy: cast all policy_uuid to string
    why: avoid unexpected behavior when using real UUID type

--- wazo_auth/database/queries/policy.py
+++ wazo_auth/database/queries/policy.py
@@ -52,14 +52,14 @@ class PolicyDAO(filters.FilterMixin, PaginatorMixin, BaseDAO):
     def dissociate_access(self, policy_uuid, access):
         filter_ = and_(
             Access.access == access,
-            PolicyAccess.policy_uuid == policy_uuid,
+            PolicyAccess.policy_uuid == str(policy_uuid),
         )
 
         query = self.session.query(Access.id_).join(PolicyAccess).filter(filter_)
         access_id = query.first()
 
         filter_ = and_(
-            PolicyAccess.policy_uuid == policy_uuid,
+            PolicyAccess.policy_uuid == str(policy_uuid),
             PolicyAccess.access_id == access_id,
         )
         self.session.query(PolicyAccess).filter(filter_).delete()
@@ -103,7 +103,7 @@ class PolicyDAO(filters.FilterMixin, PaginatorMixin, BaseDAO):
         return policy.uuid
 
     def delete(self, policy_uuid, tenant_uuids):
-        filter_ = Policy.uuid == policy_uuid
+        filter_ = Policy.uuid == str(policy_uuid)
         if tenant_uuids is not None:
             filter_ = and_(filter_, Policy.tenant_uuid.in_(tenant_uuids))
 
@@ -231,7 +231,7 @@ class PolicyDAO(filters.FilterMixin, PaginatorMixin, BaseDAO):
         config_managed=None,
         tenant_uuids=None,
     ):
-        filter_ = Policy.uuid == policy_uuid
+        filter_ = Policy.uuid == str(policy_uuid)
         if tenant_uuids is not None:
             filter_ = and_(filter_, Policy.tenant_uuid.in_(tenant_uuids))
 
@@ -265,7 +265,7 @@ class PolicyDAO(filters.FilterMixin, PaginatorMixin, BaseDAO):
     def _associate_acl(self, policy_uuid, acl):
         ids = self._create_or_find_acl(acl)
         access_policies = [
-            PolicyAccess(policy_uuid=policy_uuid, access_id=id_) for id_ in ids
+            PolicyAccess(policy_uuid=str(policy_uuid), access_id=id_) for id_ in ids
         ]
         self.session.add_all(access_policies)
 
@@ -285,7 +285,7 @@ class PolicyDAO(filters.FilterMixin, PaginatorMixin, BaseDAO):
         return result
 
     def _dissociate_all_acl(self, policy_uuid):
-        filter_ = PolicyAccess.policy_uuid == policy_uuid
+        filter_ = PolicyAccess.policy_uuid == str(policy_uuid)
         self.session.query(PolicyAccess).filter(filter_).delete()
         self.session.flush()
 
