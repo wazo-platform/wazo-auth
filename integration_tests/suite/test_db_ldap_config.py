ea6f4ceb01fe608ef752da9c92ac79807be2bb02
Alexandre Fournier <afournier@wazo.io>
2022-02-03 16:47:27 -0500

    ldap_config: add DAO

---
+++ integration_tests/suite/test_db_ldap_config.py
@@ -0,0 +1,9 @@
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from wazo_auth import exceptions
+from .helpers import fixtures, base
+from .helpers.constants import UNKNOWN_UUID, UNKNOWN_SLUG
+
+
+# TODO
---
+++ wazo_auth/database/queries/ldap_config.py
@@ -0,0 +1,25 @@
+# Copyright 2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .base import BaseDAO
+from ..models import LDAPConfig
+from ... import exceptions
+
+
+class LDAPConfigDAO(BaseDAO):
+    def get(self, tenant_uuid):
+        ldap_config = self.session.query(LDAPConfig).get(tenant_uuid)
+        if ldap_config:
+            return {
+                'tenant_uuid': ldap_config.tenant_uuid,
+                'host': ldap_config.host,
+                'port': ldap_config.port,
+                'protocol_version': ldap_config.protocol_version,
+                'protocol_security': ldap_config.protocol_security,
+                'bind_dn': ldap_config.bind_dn,
+                'bind_password': ldap_config.bind_password,
+                'user_base_dn': ldap_config.user_base_dn,
+                'user_login_attribute': ldap_config.user_login_attribute,
+                'user_email_attribute': ldap_config.user_email_attribute,
+            }
+        # XXX raise exception LDAPConfigNonExisting???
