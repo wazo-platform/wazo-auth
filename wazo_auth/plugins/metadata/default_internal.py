3c7fb4e861fa77a3436c96f8baa9768cbeb2769a
Francois Blackburn <fblackburn@wazo.community>
2022-02-07 13:04:46 -0500

    remove unused logging import

--- wazo_auth/plugins/metadata/default_internal.py
+++ wazo_auth/plugins/metadata/default_internal.py
@@ -1,11 +1,7 @@
-# Copyright 2018-2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import logging
-
 from wazo_auth import BaseMetadata
-
-logger = logging.getLogger(__name__)
 
 
 class DefaultInternal(BaseMetadata):
