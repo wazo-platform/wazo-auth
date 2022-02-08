5342eec035d6430d0f31e7230fee24ae614c6006
Alexandre Fournier <afournier@wazo.io>
2022-02-08 10:45:58 -0500

    tokens: add tenant_id to schema

--- wazo_auth/plugins/http/tokens/schemas.py
+++ wazo_auth/plugins/http/tokens/schemas.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import validates_schema
@@ -16,6 +16,7 @@ class TokenRequestSchema(BaseSchema):
     access_type = fields.String(validate=OneOf(['online', 'offline']))
     client_id = fields.String(validate=Length(min=1, max=1024))
     refresh_token = fields.String()
+    tenant_id = fields.String()
 
     @validates_schema
     def check_access_type_usage(self, data):
