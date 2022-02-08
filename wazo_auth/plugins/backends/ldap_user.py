9972646eab3ef12144c706d37f4243da56446859
Alexandre Fournier <afournier@wazo.io>
2022-02-08 13:46:13 -0500

    ldap_user: set LDAP options from config

--- wazo_auth/plugins/backends/ldap_user.py
+++ wazo_auth/plugins/backends/ldap_user.py
@@ -41,32 +41,49 @@ class LDAPUser(BaseAuthenticationBackend):
         return metadata
 
     def verify_password(self, username, password, args):
-        tenant = self._get_tenant(args['tenant_id'])  # TODO Add token POST schema validation
+        tenant = self._get_tenant(
+            args['tenant_id']
+        )  # TODO Add token POST schema validation
 
-        config = self._get_ldap_config(tenant)
+        config = self._get_ldap_config(tenant['uuid'])
         if not config:
             return False
 
-        try:
-            wazo_ldap = _WazoLDAP(config['uri'])
+        bind_dn = config.get('bind_dn')
+        bind_password = config.get('bind_password')
+        user_login_attribute = config.get('user_login_attribute')
+        user_email_attribute = config.get('user_email_attribute')
+        user_base_dn = config.get('user_base_dn')
 
-            if config['bind_dn'] and config['bind_password']:
-                if wazo_ldap.perform_bind(config['bind_dn'], config['bind_password']):
-                    user_dn = self._perform_search_dn(wazo_ldap, username)
+        try:
+            wazo_ldap = _WazoLDAP(config)
+
+            if bind_dn and bind_password:
+                if wazo_ldap.perform_bind(bind_dn, bind_password):
+                    user_dn = self._perform_search_dn(
+                        wazo_ldap,
+                        username,
+                        user_login_attribute,
+                        user_base_dn,
+                    )
                 else:
                     return False
             else:
-                user_dn = self._build_dn_with_config(username)
+                user_dn = self._build_dn_with_config(
+                    username, user_login_attribute, user_base_dn
+                )
 
             if not user_dn or not wazo_ldap.perform_bind(user_dn, password):
                 return False
 
-            user_email = self._get_user_ldap_email(wazo_ldap, user_dn)
+            user_email = self._get_user_ldap_email(
+                wazo_ldap, user_dn, user_email_attribute
+            )
             if not user_email:
                 return False
 
         except ldap.SERVER_DOWN:
-            logger.warning('LDAP : SERVER not responding on %s', config['uri'])
+            logger.warning('LDAP : SERVER not responding on %s', wazo_ldap.uri)
             return False
         except ldap.LDAPError as exc:
             logger.exception('ldap.LDAPError (%r, %r)', config, exc)
@@ -81,18 +98,8 @@ class LDAPUser(BaseAuthenticationBackend):
 
         return True
 
-    def _build_uri(self, protocol_security, port, host):
-        scheme = 'ldaps' if protocol_security == 'ldaps' else 'ldap'
-        return f'{scheme}://{host}:{port}'
-
     def _get_ldap_config(self, tenant_uuid):
-        config = self._ldap_service.get(tenant_uuid)
-        config['uri'] = self._build_uri(
-            config['protocol_security'],
-            config['port'],
-            config['host'],
-        )
-        return config
+        return self._ldap_service.get(tenant_uuid)
 
     def _get_tenant(self, tenant_id):
         return self._tenant_service.get_by_uuid_or_slug(None, tenant_id)
@@ -107,48 +114,67 @@ class LDAPUser(BaseAuthenticationBackend):
             return
         return user['uuid']
 
-    def _build_dn_with_config(self, login):
+    def _build_dn_with_config(self, login, user_login_attribute, user_base_dn):
         login_esc = escape_dn_chars(login)
-        return '{}={},{}'.format(
-            self.user_login_attribute, login_esc, self.user_base_dn
-        )
+        return '{}={},{}'.format(user_login_attribute, login_esc, user_base_dn)
 
-    def _get_user_ldap_email(self, wazo_ldap, user_dn):
+    def _get_user_ldap_email(self, wazo_ldap, user_dn, user_email_attribute):
         _, obj = wazo_ldap.perform_search(
-            user_dn, ldap.SCOPE_BASE, attrlist=[self.user_email_attribute]
+            user_dn, ldap.SCOPE_BASE, attrlist=[user_email_attribute]
         )
-        email = obj.get(self.user_email_attribute, None)
+        email = obj.get(user_email_attribute, None)
         email = email[0] if isinstance(email, list) else email
         if not email:
             logger.debug('LDAP : No email found for the user DN: %s', user_dn)
             return
         return email.decode('utf-8')
 
-    def _perform_search_dn(self, wazo_ldap, username):
+    def _perform_search_dn(
+        self, wazo_ldap, username, user_login_attribute, user_base_dn
+    ):
         username_esc = escape_filter_chars(username)
-        filterstr = '{}={}'.format(self.user_login_attribute, username_esc)
+        filterstr = '{}={}'.format(user_login_attribute, username_esc)
         dn, _ = wazo_ldap.perform_search(
-            self.user_base_dn, ldap.SCOPE_SUBTREE, filterstr=filterstr, attrlist=['']
+            user_base_dn, ldap.SCOPE_SUBTREE, filterstr=filterstr, attrlist=['']
         )
         if not dn:
             logger.debug(
                 'LDAP : No user DN for user_base dn: %s and filterstr: %s',
-                self.user_base_dn,
+                user_base_dn,
                 filterstr,
             )
         return dn
 
 
 class _WazoLDAP:
-    def __init__(self, uri):
-        self.uri = uri
-        self.ldapobj = self._create_ldap_obj(self.uri)
+    def __init__(self, config):
+        self.config = config
+        self.uri = self._build_uri(
+            config['protocol_security'], config['port'], config['host']
+        )
+        self.ldapobj = self._create_ldap_obj(self.config)
 
-    def _create_ldap_obj(self, uri):
-        ldapobj = ldap.initialize(uri)
+    def _build_uri(self, protocol_security, port, host):
+        scheme = 'ldaps' if protocol_security == 'ldaps' else 'ldap'
+        return f'{scheme}://{host}:{port}'
+
+    def _create_ldap_obj(self, config):
+        ldapobj = ldap.initialize(self.uri)
         ldapobj.set_option(ldap.OPT_REFERRALS, 0)
         ldapobj.set_option(ldap.OPT_NETWORK_TIMEOUT, 2)
         ldapobj.set_option(ldap.OPT_TIMEOUT, 2)
+
+        version_map = {
+            2: ldap.VERSION2,
+            3: ldap.VERSION3,
+        }
+        ldapobj.set_option(
+            ldap.OPT_PROTOCOL_VERSION, version_map.get(config['protocol_version'], 3)
+        )
+
+        if config['protocol_security'] == 'tls':
+            ldapobj.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
+            ldapobj.start_tls_s()
         return ldapobj
 
     def perform_bind(self, username, password):
