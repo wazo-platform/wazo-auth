260fbeb148b2792fee291901c8cea49b36defa62
Pascal Cadotte Michaud <pcm@wazo.io>
2023-02-09 10:56:50 -0500

    metrics: add prometheus metrics

--- wazo_auth/plugins/http/metric/plugin.py
+++ wazo_auth/plugins/http/metric/plugin.py
@@ -1,13 +1,11 @@
 # Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from . import http
+from prometheus_flask_exporter import PrometheusMetrics
 
 
 class Plugin:
     def load(self, dependencies):
         api = dependencies['api']
-        api.add_resource(
-            http.MetricResource,
-            '/metrics',
-        )
+
+        self.metrics = PrometheusMetrics(api.app, path=f'{api.prefix}/metrics')
