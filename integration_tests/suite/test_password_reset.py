68f867e537dcd9c624cc384724dcc7b947571b35
Charles Langlois <charles.langlois@wazo.io>
2023-05-15 11:35:05 -0400

    Added integration test for password reset link at-most-once behavior

--- integration_tests/suite/test_password_reset.py
+++ integration_tests/suite/test_password_reset.py
@@ -50,13 +50,39 @@ class TestResetPassword(base.APIIntegrationTest):
             assert_no_error(user_client.token.new, 'wazo_user', expiration=1)
 
     def _update_password_from_email(self, raw_email, password):
+        token, user_uuid = self._get_reset_token_from_email(raw_email)
+        return self.client.users.set_password(user_uuid, password, token)
+
+    def _get_reset_token_from_email(self, raw_email):
         headers, body = raw_email.split('\n\n', 1)
         email_fields = yaml.safe_load(body)
 
         token = email_fields['token']
         user_uuid = email_fields['user_uuid']
 
-        return self.client.users.set_password(user_uuid, password, token)
+        return token, user_uuid
+
+    @fixtures.http.user(username='user@example.com', email_address='user@example.com')
+    def test_password_reset_unipotence(self, foo):
+        self.clean_emails()
+        self.client.users.reset_password(username=foo['username'])
+        emails = self.get_emails()
+
+        assert_that(
+            emails,
+            contains_inanyorder(
+                contains_string('username: user@example.com'),
+            ),
+        )
+        new_password = '5ecr37'
+        email = next(emails)
+        token, user_uuid = self._get_reset_token_from_email(email)
+        self.client.users.set_password(user_uuid, password, token)
+        # second time token has been revoked
+        new_new_password = 'abcdef5'
+        with pytest.raises(HTTPError) as exc_info:
+            self.client.users.set_password(user_uuid, new_new_password, token)
+        assert exc_info.status == 401
 
     @fixtures.http.user(username='foobar')
     def test_set_password(self, user):
