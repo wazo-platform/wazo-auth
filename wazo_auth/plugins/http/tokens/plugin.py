7ca94faebb0447766a6839083d1bf22eae2dcc01
Pascal Cadotte Michaud <pcm@wazo.io>
2024-05-09 14:19:04 -0400

    token: fix args for each token http handlers

--- wazo_auth/plugins/http/tokens/plugin.py
+++ wazo_auth/plugins/http/tokens/plugin.py
@@ -7,52 +7,57 @@ from . import http
 class Plugin:
     def load(self, dependencies):
         api = dependencies['api']
-        args = (
+        token_args = (
             dependencies['token_service'],
             dependencies['user_service'],
             dependencies['saml_service'],
             dependencies['authentication_service'],
             dependencies['backends'],
         )
+        refresh_token_args = (
+            dependencies['token_service'],
+            dependencies['user_service'],
+            dependencies['authentication_service'],
+        )
 
         api.add_resource(
             http.Tokens,
             '/token',
-            resource_class_args=args,
+            resource_class_args=token_args,
         )
         api.add_resource(
             http.Token,
             '/token/<uuid:token_uuid>',
-            resource_class_args=args,
+            resource_class_args=token_args,
         )
         api.add_resource(
             http.TokenScopesCheck,
             '/token/<uuid:token_uuid>/scopes/check',
-            resource_class_args=args,
+            resource_class_args=token_args,
         )
         api.add_resource(
             http.RefreshTokens,
             '/tokens',
-            resource_class_args=args,
+            resource_class_args=refresh_token_args,
         )
         api.add_resource(
             http.UserRefreshTokens,
             '/users/<uuid:user_uuid>/tokens',
-            resource_class_args=args,
+            resource_class_args=refresh_token_args,
         )
         api.add_resource(
             http.UserRefreshToken,
             '/users/<uuid:user_uuid>/tokens/<string:client_id>',
-            resource_class_args=args,
+            resource_class_args=refresh_token_args,
         )
 
         api.add_resource(
             http.UserMeRefreshTokens,
             '/users/me/tokens',
-            resource_class_args=args,
+            resource_class_args=refresh_token_args,
         )
         api.add_resource(
             http.UserMeRefreshToken,
             '/users/me/tokens/<string:client_id>',
-            resource_class_args=args,
+            resource_class_args=refresh_token_args,
         )
