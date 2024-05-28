ed2f37b2c772d53397669d8b1bd1a7726959acca
Jirka HLAVACEK <jirka.hlavacek@gmail.com>
2024-05-28 11:31:56 +0200

    Add SAML missing parameters tests
    - requires SAML enabled wazo-auth-client

---
+++ integration_tests/suite/test_saml.py
@@ -0,0 +1,39 @@
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .helpers import base
+from .helpers.base import assert_http_error
+
+@base.use_asset('base')
+class TestSAML(base.APIIntegrationTest):
+    def test_sso_missing_redirect_url(self):
+        assert_http_error(
+            400,
+            self.client.saml.sso,
+            domain='only_domain',
+            redirect_url=None            
+        )
+        
+    def test_sso_missing_domain(self):
+        assert_http_error(
+            400,
+            self.client.saml.sso,
+            domain=None,
+            redirect_url='https://example.com/acs'
+        )
+
+    def test_acs_missing_response(self):
+        assert_http_error(
+            400,
+            self.client.saml.acs,
+            response=None,
+            token='token'
+        )
+        
+    def test_acs_missing_token(self):
+        assert_http_error(
+            400,
+            self.client.saml.acs,
+            response='response',
+            token=None            
+        )
