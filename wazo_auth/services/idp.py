b8d81ac45483252d00e5f764eecfaf31e8af6c8e
Pascal Cadotte Michaud <pcm@wazo.io>
2024-06-04 15:09:04 -0400

    idp: refactor user update code in idp service
    avoid leaking dao session to the service

--- wazo_auth/services/idp.py
+++ wazo_auth/services/idp.py
@@ -6,14 +6,10 @@ from wazo_auth.services.helpers import BaseService
 
 class IDPService(BaseService):
     def add_user(self, idp_type, user_uuid):
-        user = self._dao.user.get(user_uuid)
-        user.authentication_method = idp_type
-        self._dao.user.session.flush()
-        return user
+        return self._dao.user.update(user_uuid, authentication_method=idp_type)
 
     def remove_user(self, idp_type, user_uuid):
         user = self._dao.user.get(user_uuid)
         if user.authentication_method == idp_type:
-            user.authentication_method = 'default'
-            self._dao.user.session.flush()
+            return self.add_user('default', user_uuid)
         return user
