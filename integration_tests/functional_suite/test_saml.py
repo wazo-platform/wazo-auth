f8c0d155c59f56c5365a6eb54432a982ba3326b1
Jirka HLAVACEK <jirka.hlavacek@gmail.com>
2024-09-18 11:41:21 +0200

    saml: add functional test logout after restart

--- integration_tests/functional_suite/test_saml.py
+++ integration_tests/functional_suite/test_saml.py
@@ -236,3 +236,28 @@ class TestSamlService(SAMLIntegrationTest):
 
         self._get_token_using_saml_session_id(self.page, 'token-already-used')
         expect(self.page.locator("#custom_id_token")).to_contain_text("Failed")
+
+    @pytest.mark.only_browser("chromium")
+    @pytest.mark.browser_context_args(
+        timezone_id="Europe/London", locale="en-GB", ignore_https_errors=True
+    )
+    def test_logout_after_restart(self) -> None:
+        self._click_login(self.page)
+        self._login(self.page, self.login, self.password)
+        expect(self.page.locator("h1"), "SSO handling failed").to_contain_text(
+            "Wazo SAML Post ACS handling"
+        )
+        expect(
+            self.page.locator("#token"), "Failed to retrieve token"
+        ).not_to_contain_text("not yet known")
+        expect(
+            self.page.locator("#refresh"), "Failed to retrieve a refresh token"
+        ).not_to_contain_text("not yet known")
+
+        self.restart_auth()
+        self._logout(self.page)
+        expect(self.page).to_have_url(
+            'https://app.wazo.local/postacs.html?logged_out=true', timeout=20000
+        )
+        expect(self.page.locator("#token")).to_contain_text("Failed")
+        expect(self.page.locator("#refresh")).to_contain_text("Failed")
